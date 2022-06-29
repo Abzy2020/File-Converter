804cf9d6e6e294dff131d36857e1fd476bae4e77
Alexander <alexanderbouzy@gmail.com>
2022-06-29 19:06:51 -0400

    change converter

--- converter.py
+++ converter.py
@@ -1,19 +1,23 @@
 from file_converter import FileConverter
 
-print('Enter absolute path to file (dont forget file extension): ')
-convert_path = input()
-print('Enter the current file type: ')
-before_convert = input()
-print('Enter file type you wish to change it to: ')
-after_convert = input()
-print('Name the file: ')
-new_file_name = input()
+def main():
+    print('Enter absolute path to file (dont forget file extension): ')
+    convert_path = input()
+    print('Enter the current file type: ')
+    before_convert = input()
+    print('Enter file type you wish to change it to: ')
+    after_convert = input()
+    print('Name the file: ')
+    new_file_name = input()
 
-conversion = FileConverter(
-                before_convert=before_convert,
-                after_convert=after_convert,
-                convert_path=convert_path,
-                new_name=new_file_name)
+    conversion = FileConverter(
+                    before_convert=before_convert,
+                    after_convert=after_convert,
+                    convert_path=convert_path,
+                    new_name=new_file_name)
 
-conversion.upload_file()
-conversion.convert_file()
+    conversion.upload_file()
+    conversion.convert_file()
+
+if __name__ == '__main__':
+    main()
